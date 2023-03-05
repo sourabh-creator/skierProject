5ab751d5bfaffe926100396bfa16b058d6b56512
Sourabh <sourabhchaturvedi07@gmail.com>
2023-03-05 00:34:39 -0500

    Modified test output to include throughput , avg, min , max, 99th percentile data

--- src/test/java/com/skier/skiersproject/MultiThreadClient.java
+++ src/test/java/com/skier/skiersproject/MultiThreadClient.java
@@ -11,8 +11,11 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,12 +29,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MultiThreadClient {
     FileWriter writer;
     Random random = new Random();
+    File file = new File("./latencies.csv");
 
 
     @Test
     public void testDoPostConcurrentConnections() throws Exception {
 
         try {
+            boolean deleted = file.delete();
+            if (deleted) {
+                System.out.println("File deleted successfully");
+            } else {
+                System.out.println("Failed to delete the file");
+            }
+            List<Long> latancies = new ArrayList<Long>();
             writer = new FileWriter("./latencies.csv");
             writer.append("Latency, Status Code\n");
             writer.flush();
@@ -49,7 +60,7 @@ public class MultiThreadClient {
                     HttpPost httpPost = new HttpPost("http://localhost:8080/skier/addSkier");
                     try {
                         long startTime = System.currentTimeMillis();
-                        Skier skier = new Skier(random.nextInt(10000-1) + 1, random.nextInt(10-1) + 1, random.nextInt(40-1) + 1, 2022, 1, random.nextInt(360-1) + 1);
+                        Skier skier = new Skier(random.nextInt(10000 - 1) + 1, random.nextInt(10 - 1) + 1, random.nextInt(40 - 1) + 1, 2022, 1, random.nextInt(360 - 1) + 1);
                         ObjectMapper objectMapper = new ObjectMapper();
                         String json = objectMapper.writeValueAsString(skier);
                         StringEntity requestEntity = new StringEntity(json);
@@ -59,6 +70,9 @@ public class MultiThreadClient {
                         CloseableHttpResponse response = httpClient.execute(httpPost);
                         long endTime = System.currentTimeMillis();
                         long timeTaken = endTime - startTime;
+                        if (timeTaken > 0) {
+                            latancies.add(timeTaken);
+                        }
                         writer.append(timeTaken + ", " + response.getStatusLine().getStatusCode() + "\n");
                         writer.flush();
                         if (response.getStatusLine().getStatusCode() == 200) {
@@ -76,6 +90,11 @@ public class MultiThreadClient {
 
             assertEquals(10000, successfulRequests.get());
             assertTrue(successfulRequests.get() > 100);
+            System.out.println("Average Latency: " + latancies.stream().mapToLong(Long::longValue).average().getAsDouble() + "ms");
+            System.out.println("Max Latency: " + latancies.stream().mapToLong(Long::longValue).max().getAsLong() + "ms");
+            System.out.println("Min Latency: " + latancies.stream().mapToLong(Long::longValue).min().getAsLong() + "ms");
+            System.out.println("Throughput: " + (10000 / latancies.stream().mapToLong(Long::longValue).average().getAsDouble()) + " requests per second");
+            System.out.println("99th Percentile Latency: " + latancies.stream().mapToLong(Long::longValue).sorted().skip((long) (latancies.size() * 0.99)).findFirst().getAsLong() + "ms");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
